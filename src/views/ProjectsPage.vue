20f39c06c9d2531e0de652eb1d69e9d92d3ee4ae
Karlo <alano.juankarlo@gmail.com>
2026-06-17 14:17:48 +0800

    fixed the missing li tags in the projects section

--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -89,47 +89,50 @@
               <div>
                 <h1 class="font-monospace font-bold opacity-80">Challenges Encountered:</h1>
                 <ul>
-                  > The project heavily uses the Google Maps API for the function of its main
-                  itinerary generator. Managing API usage, restricting access, and streamlining API
-                  request pipelines was initially a challenge not because it was hard, rather it was
-                  numerous. Navigating through the Google Cloud Console was also confusing at first
-                  due to the many menus, navigation bars, and clickable links and buttons.
+                  <li>
+                    > The project heavily uses the Google Maps API for the function of its main
+                    itinerary generator. Managing API usage, restricting access, and streamlining API
+                    request pipelines was initially a challenge not because it was hard, rather it was
+                    numerous. Navigating through the Google Cloud Console was also confusing at first
+                    due to the many menus, navigation bars, and clickable links and buttons.
+                  </li>
+                  <li>
+                    > Building a cohesive and well put together User Interface requires careful
+                    planning, foresight, and theme. With no expert in UI/UX, we struggled with this
+                    for the initial stages of the project.
+                  </li>
+                  <li>
+                    > Collaborating with a team of three required careful distribution of workload. In
+                    the initial stages of the project, this regard was not focused on which led to
+                    eventual burnout and lack of direction.
+                  </li>
                 </ul>
-                <ul>
-                  > Building a cohesive and well put together User Interface requires careful
-                  planning, foresight, and theme. With no expert in UI/UX, we struggled with this
-                  for the initial stages of the project.
-                </ul>
-                <ul>
-                  > Collaborating with a team of three required careful distribution of workload. In
-                  the initial stages of the project, this regard was not focused on which led to
-                  eventual burnout and lack of direction.
-                </ul>
-                <ul></ul>
               </div>
               <div class="divider-s"></div>
               <div>
                 <h1 class="font-monospace font-bold opacity-80">Lessons Learned:</h1>
                 <ul>
-                  > In order to reduce API costs, I have decided to implement a JSON cache of store
-                  data. This is updated biweekly through a yaml script on Github Actions. This not
-                  only reduced usage - it also made for a more streamlined pipeline and gave me
-                  experience into writing automation pipelines.
-                </ul>
-                <ul>
-                  > Around the middle of the project's completion, one of my teammates proposed a
-                  completely different design language. This new design language turned out to fit
-                  the general vibe of the website better and thus a pivot was made to fully adopt
-                  the change. Furthermore, using PrimeVue, a UI component library made for the
-                  Vue.js framework, helped the project massively as it allowed fast prototyping and
-                  the establishment of a central design language.
-                </ul>
-                <ul>
-                  > Streamlining the Git pipeline and designating tasks and modules to my teammates
-                  allowed us to finish our project in time. We established commit guidelines that
-                  followed common practices and shared workload evenly. We assigned each other
-                  dedicated modules to focus and work on, which allowed us to hit our established
-                  timelines.
+                  <li>
+                    > In order to reduce API costs, I have decided to implement a JSON cache of store
+                    data. This is updated biweekly through a yaml script on Github Actions. This not
+                    only reduced usage - it also made for a more streamlined pipeline and gave me
+                    experience into writing automation pipelines.
+                  </li>
+                  <li>
+                    > Around the middle of the project's completion, one of my teammates proposed a
+                    completely different design language. This new design language turned out to fit
+                    the general vibe of the website better and thus a pivot was made to fully adopt
+                    the change. Furthermore, using PrimeVue, a UI component library made for the
+                    Vue.js framework, helped the project massively as it allowed fast prototyping and
+                    the establishment of a central design language.
+                  </li>
+                  <li>
+                    > Streamlining the Git pipeline and designating tasks and modules to my teammates
+                    allowed us to finish our project in time. We established commit guidelines that
+                    followed common practices and shared workload evenly. We assigned each other
+                    dedicated modules to focus and work on, which allowed us to hit our established
+                    timelines.
+                  </li>
                 </ul>
               </div>
             </div>
